aa3bc7b63dcb7141b469709425029e5a2f5dc846
jurck222 <56190152+jurck222@users.noreply.github.com>
2024-03-15 21:20:40 +0100

    :lipstick: Added page for quest info

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { AddQuestComponent } from './components/add-quest/add-quest.component';
-import { QuestComponent } from './components/quest/quest.component';
 import { VillageQuestsComponent } from './components/village-quests/village-quests.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 
@@ -8,5 +7,9 @@ export const routes: Routes = [
   { path: 'add-quest', component: AddQuestComponent },
   { path: 'village-quests', component: VillageQuestsComponent },
   { path: '', component: WelcomeComponent },
-  { path: 'quest/:id', component: QuestComponent },
+  {
+    path: 'quest/:id',
+    title: 'Quest information',
+    loadComponent: () => import('./components/quest/quest.component').then(m => m.QuestComponent),
+  },
 ];
--- frontend/src/app/components/quest/quest.component.ts
+++ frontend/src/app/components/quest/quest.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit, input } from '@angular/core';
+import { DatePipe, NgClass } from '@angular/common';
+import { Component, inject, input, numberAttribute } from '@angular/core';
+import { computedAsync } from 'ngxtension/computed-async';
+import { QuestService } from '../../services/quest.service';
 
 @Component({
   selector: 'app-quest',
   standalone: true,
-  imports: [],
+  imports: [DatePipe, NgClass],
   templateUrl: './quest.component.html',
   styleUrl: './quest.component.css',
 })
-export class QuestComponent implements OnInit {
-  questId = input();
+export class QuestComponent {
+  readonly #questService = inject(QuestService);
 
-  ngOnInit(): void {
-    console.log(this.questId());
+  questId = input(0, { alias: 'id', transform: numberAttribute }); // url param id
+
+  questInfo = computedAsync(() => (this.questId() ? this.#getQuestInfo(this.questId()) : null));
+
+  #getQuestInfo(id: number) {
+    return this.#questService.getQuestById(id);
   }
 }
